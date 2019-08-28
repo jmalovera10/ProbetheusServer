3112c931778b3746033341211d95546e9c2047ce
jmalovera10 <jma.lovera10@uniandes.edu.co>
2019-08-27 21:51:02 -0500

    Tests added

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,9 +6,9 @@ export default class Home extends Component {
         return (
             <div className="jumbotron">
                 <h1 className="display-4">Probetheus</h1>
-                <p className="lead">Una aplicación para monitorear las aguas y empoderar la gente de Samacá</p>
+                <p className="lead">Una aplicación para monitorear las aguas y empoderar a la gente de Samacá</p>
                 <hr className="my-4"/>
-                <p>Ayudamos a los habitantes de samacá a tener control sobre sus recursos hídricos.</p>
+                <p>Ayudamos a los habitantes de Samacá a tener control sobre sus recursos hídricos.</p>
             </div>
         );
     }
