c6eb7dae85c0e255e31e0f2bb8abbe80e9929a15
jmalovera10 <jma.lovera10@uniandes.edu.co>
2019-09-20 07:35:35 -0500

    Changes on dependencies and user ID assignation

--- api/UserAPI.js
+++ api/UserAPI.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+const sha256 = require('sha256');
 
 /**
  * Method that adds user
@@ -6,25 +7,31 @@ const mysql = require('mysql');
  * @param res
  */
 exports.postUser = (req, res) => {
-    let user = req.body;
-    let connection = mysql.createConnection({
-        host: process.env.MYSQL_HOST,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE
-    });
-
-    connection.connect();
-
-    connection.query('INSERT INTO USERS(ID, NAME, SEX, SCORE) VALUES (?,?,?,?)', [user.ID, user.NAME, user.SEX, 0],
-        function (err, rows, fields) {
-            if (err){
-                console.log(err);
-                res.status(500).send(err);
-            }
-            console.log('The solution is: ', rows);
-            res.status(200).send(req.body);
+    try {
+        let user = req.body;
+        let connection = mysql.createConnection({
+            host: process.env.MYSQL_HOST,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASSWORD,
+            database: process.env.MYSQL_DATABASE
         });
 
-    connection.end()
+        connection.connect();
+
+        const ID = sha256(`${Date.now()}`);
+
+        connection.query('INSERT INTO USERS(ID, NAME, SEX, SCORE) VALUES (?,?,?,?)', [ID, user.NAME, user.SEX, user.SCORE],
+            function (err, rows, fields) {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send(err);
+                }
+                console.log('USER CREATED, ID: ', ID);
+                res.status(200).send({ID});
+            });
+
+        connection.end()
+    }catch (e) {
+        console.log(e);
+    }
 };
